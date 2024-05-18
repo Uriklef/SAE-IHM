bdee1831d943e8ff9774980072c8dc524df894a3
Rayan <contact.rayanmeri@gmail.com>
2024-05-18 22:53:08 +0200

    Ajoutez des fichiers projet.

---
+++ SAE IHM/Form1.vb
@@ -0,0 +1,12 @@
+﻿Public Class Form1
+    Private Sub btnQuitter_Click(sender As Object, e As EventArgs) Handles btnQuitter.Click
+        Dim resultat As MsgBoxResult = MsgBox("Voulez vous quitter l'application ?", vbYesNo)
+        If resultat = MsgBoxResult.Yes Then
+            MsgBox("Arrêt de l'application")
+            Me.Hide()
+        ElseIf resultat = MsgBoxResult.No Then
+            Me.Hide()
+            Me.Show()
+        End If
+    End Sub
+End Class
