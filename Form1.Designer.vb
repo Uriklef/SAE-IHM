b4e4492eb0151d038abe9b041d3df64d2fb6d1ee
Francuz4520 <153096201+Francuz4520@users.noreply.github.com>
2024-05-23 22:28:00 +0200

    Ajout saisie nom et bouton terminer

---
+++ Form1.Designer.vb
@@ -0,0 +1,168 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form remplace la méthode Dispose pour nettoyer la liste des composants.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requise par le Concepteur Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'REMARQUE : la procédure suivante est requise par le Concepteur Windows Form
+    'Elle peut être modifiée à l'aide du Concepteur Windows Form.  
+    'Ne la modifiez pas à l'aide de l'éditeur de code.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.LRegle = New System.Windows.Forms.Label()
+        Me.LTitre = New System.Windows.Forms.Label()
+        Me.BRetour = New System.Windows.Forms.Button()
+        Me.LScores = New System.Windows.Forms.Label()
+        Me.PMenu = New System.Windows.Forms.Panel()
+        Me.BLeave = New System.Windows.Forms.Button()
+        Me.BRegle = New System.Windows.Forms.Button()
+        Me.BLeaderBoard = New System.Windows.Forms.Button()
+        Me.BJouer = New System.Windows.Forms.Button()
+        Me.PMenu.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'LRegle
+        '
+        Me.LRegle.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LRegle.Location = New System.Drawing.Point(62, 198)
+        Me.LRegle.Name = "LRegle"
+        Me.LRegle.Size = New System.Drawing.Size(800, 199)
+        Me.LRegle.TabIndex = 3
+        Me.LRegle.Text = resources.GetString("LRegle.Text")
+        '
+        'LTitre
+        '
+        Me.LTitre.AutoSize = True
+        Me.LTitre.Font = New System.Drawing.Font("Papyrus", 28.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LTitre.Location = New System.Drawing.Point(277, 60)
+        Me.LTitre.Name = "LTitre"
+        Me.LTitre.Size = New System.Drawing.Size(321, 74)
+        Me.LTitre.TabIndex = 2
+        Me.LTitre.Text = "SUDOKU"
+        Me.LTitre.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'BRetour
+        '
+        Me.BRetour.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BRetour.Location = New System.Drawing.Point(57, 503)
+        Me.BRetour.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.BRetour.Name = "BRetour"
+        Me.BRetour.Size = New System.Drawing.Size(125, 44)
+        Me.BRetour.TabIndex = 8
+        Me.BRetour.Text = "Retour"
+        Me.BRetour.UseVisualStyleBackColor = True
+        '
+        'LScores
+        '
+        Me.LScores.AutoSize = True
+        Me.LScores.Cursor = System.Windows.Forms.Cursors.Default
+        Me.LScores.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LScores.Location = New System.Drawing.Point(289, 180)
+        Me.LScores.Name = "LScores"
+        Me.LScores.Size = New System.Drawing.Size(309, 58)
+        Me.LScores.TabIndex = 9
+        Me.LScores.Text = "Joueurs         |         Temps" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "-------------------------------------" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+        Me.LScores.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'PMenu
+        '
+        Me.PMenu.BackColor = System.Drawing.Color.Transparent
+        Me.PMenu.Controls.Add(Me.BLeave)
+        Me.PMenu.Controls.Add(Me.BRegle)
+        Me.PMenu.Controls.Add(Me.BLeaderBoard)
+        Me.PMenu.Controls.Add(Me.BJouer)
+        Me.PMenu.Cursor = System.Windows.Forms.Cursors.Default
+        Me.PMenu.Location = New System.Drawing.Point(294, 153)
+        Me.PMenu.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.PMenu.Name = "PMenu"
+        Me.PMenu.Size = New System.Drawing.Size(285, 327)
+        Me.PMenu.TabIndex = 10
+        '
+        'BLeave
+        '
+        Me.BLeave.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BLeave.Location = New System.Drawing.Point(53, 254)
+        Me.BLeave.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.BLeave.Name = "BLeave"
+        Me.BLeave.Size = New System.Drawing.Size(183, 58)
+        Me.BLeave.TabIndex = 8
+        Me.BLeave.Text = "Quitter"
+        Me.BLeave.UseVisualStyleBackColor = True
+        '
+        'BRegle
+        '
+        Me.BRegle.Cursor = System.Windows.Forms.Cursors.Default
+        Me.BRegle.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BRegle.Location = New System.Drawing.Point(53, 176)
+        Me.BRegle.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.BRegle.Name = "BRegle"
+        Me.BRegle.Size = New System.Drawing.Size(183, 58)
+        Me.BRegle.TabIndex = 6
+        Me.BRegle.Text = "Règles"
+        Me.BRegle.UseVisualStyleBackColor = True
+        '
+        'BLeaderBoard
+        '
+        Me.BLeaderBoard.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BLeaderBoard.Location = New System.Drawing.Point(53, 91)
+        Me.BLeaderBoard.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.BLeaderBoard.Name = "BLeaderBoard"
+        Me.BLeaderBoard.Size = New System.Drawing.Size(183, 62)
+        Me.BLeaderBoard.TabIndex = 5
+        Me.BLeaderBoard.Text = "Tableau des scores"
+        Me.BLeaderBoard.UseVisualStyleBackColor = True
+        '
+        'BJouer
+        '
+        Me.BJouer.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BJouer.Location = New System.Drawing.Point(53, 14)
+        Me.BJouer.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.BJouer.Name = "BJouer"
+        Me.BJouer.Size = New System.Drawing.Size(183, 52)
+        Me.BJouer.TabIndex = 4
+        Me.BJouer.Text = "Lancer une partie"
+        Me.BJouer.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(919, 559)
+        Me.Controls.Add(Me.PMenu)
+        Me.Controls.Add(Me.LScores)
+        Me.Controls.Add(Me.BRetour)
+        Me.Controls.Add(Me.LRegle)
+        Me.Controls.Add(Me.LTitre)
+        Me.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.PMenu.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents LRegle As Label
+    Friend WithEvents LTitre As Label
+    Friend WithEvents BRetour As Button
+    Friend WithEvents LScores As Label
+    Friend WithEvents PMenu As Panel
+    Friend WithEvents BLeave As Button
+    Friend WithEvents BRegle As Button
+    Friend WithEvents BLeaderBoard As Button
+    Friend WithEvents BJouer As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,51 @@
+﻿Public Class Form1
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        LRegle.Hide()
+        BRetour.Hide()
+        LScores.Hide()
+    End Sub
+
+    Private Sub Button_MouseHover(sender As Object, e As EventArgs) Handles BRetour.MouseEnter, BJouer.MouseEnter, BLeaderBoard.MouseEnter, BRegle.MouseEnter, BLeave.MouseEnter
+        sender.BackColor = Color.Silver
+    End Sub
+
+    Private Sub Button_MouseLeave(sender As Object, e As EventArgs) Handles BRetour.MouseLeave, BJouer.MouseLeave, BLeaderBoard.MouseLeave, BRegle.MouseLeave, BLeave.MouseLeave
+        sender.BackColor = Color.Transparent
+    End Sub
+
+    Private Sub BRegle_Click(sender As Object, e As EventArgs) Handles BRegle.Click
+        PMenu.Hide()
+        LRegle.Show()
+        BRetour.Show()
+        LTitre.Text = "REGLES"
+    End Sub
+
+    Private Sub BRetour_Click(sender As Object, e As EventArgs) Handles BRetour.Click
+        LRegle.Hide()
+        BRetour.Hide()
+        LScores.Hide()
+        PMenu.Show()
+        LTitre.Text = "SUDOKU"
+    End Sub
+
+    Private Sub BLeave_Click(sender As Object, e As EventArgs) Handles BLeave.Click
+        Dim MsgResult As MsgBoxResult = MsgBox("Êtes vous sur de vouloir quitter l'application ?", MsgBoxStyle.YesNo)
+        If MsgResult = vbYes Then
+            Me.Close()
+        End If
+    End Sub
+
+    Private Sub BLeaderBoard_Click(sender As Object, e As EventArgs) Handles BLeaderBoard.Click
+        PMenu.Hide()
+        LScores.Show()
+        BRetour.Show()
+        LTitre.Text = "SCORES"
+    End Sub
+
+    Private Sub BJouer_Click(sender As Object, e As EventArgs) Handles BJouer.Click
+        Dim formulaireJeu As New Form2()
+        formulaireJeu.Show()
+        Me.Hide()
+    End Sub
+End Class
